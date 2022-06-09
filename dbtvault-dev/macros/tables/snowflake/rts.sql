6c7c9b569f81cd9253c45211621b175de94638f2
Carl Boyce <CARL.BOYCE@DATA-VAULT.COM>
2022-06-09 12:28:13 +0000

    WIP clean up

--- dbtvault-dev/macros/tables/snowflake/rts.sql
+++ dbtvault-dev/macros/tables/snowflake/rts.sql
@@ -1,6 +1,3 @@
-
-
-
 {%- macro rts(src_pk, src_ldts, src_source, src_hashdiff, source_model) -%}
     {{- adapter.dispatch('rts', 'dbtvault')(src_pk=src_pk,
                                             src_ldts=src_ldts,
@@ -16,6 +13,7 @@
 {%- set src_ldts = dbtvault.escape_column_names(src_ldts) -%}
 {%- set src_source = dbtvault.escape_column_names(src_source) -%}
 {%- set src_hashdiff = dbtvault.escape_column_names(src_hashdiff) -%}
+{%- set source_cols = dbtvault.expand_column_list(columns=[src_pk, src_ldts, src_source, src_hashdiff]) -%}
 
 
 {{ dbtvault.prepend_generated_by() }}
@@ -24,101 +22,59 @@
     {%- set source_model = [source_model] -%}
 {%- endif %}
 
-
-
 {{ 'WITH ' }}
- {%- for src in source_model %}
-
-
---  SELECT DISTINCT
---     {{ dbtvault.prefix([src_pk], 's') }},
---      s.{{ src_ldts }},
---      s.{{ src_source }},
---     s.{{ src_hashdiff }} AS HASHDIFF
---     FROM {{ ref(src) }} as s
---
---
---
---     WHERE NOT EXISTS
---     (SELECT 1
---     FROM
---     (SELECT {{ src_pk }},
---             {{ 'HASHDIFF' }},
---             ROW_NUMBER() OVER
---     (PARTITION BY {{ src_pk }}
---     ORDER BY {{ src_ldts }}) AS DV_RNK
---
---         FROM {{ ref(src) }}
---         QUALIFY DV_RNK = 1) CUR
---          WHERE {{ src_pk }} = CUR.{{ src_pk }}
---         AND {{ 'HASHDIFF' }} = CUR.{{ 'HASHDIFF' }})
--- --         AND {{ dbtvault.multikey(src_pk, prefix='s', condition='IS NOT NULL') }}
-
-
-
-------------------
-
-
+{%- for src in source_model %}
 
 y AS (
-
-    SELECT DISTINCT {{ dbtvault.prefix([src_pk], 's') }}, s.{{ src_ldts }}, s.{{ src_source }}, s.{{ src_hashdiff }} AS HASHDIFF
-
-
-
-
+    SELECT DISTINCT {{ dbtvault.prefix([src_pk], 's') }},
+                    s.{{ src_ldts }},
+                    s.{{ src_source }},
+                    s.{{ src_hashdiff }}
     FROM {{ ref(src) }} as s
-        WHERE {{ dbtvault.multikey(src_pk, prefix='s', condition='IS NOT NULL') }}
-
-
+    WHERE {{ dbtvault.multikey(src_pk, prefix='s', condition='IS NOT NULL') }}
 ),
 
 {%- endfor %}
 
 x as (
     SELECT * FROM y
-    --          {%- if dbtvault.is_vault_insert_by_period() or is_incremental() %}
-    WHERE NOT EXISTS
-    (SELECT 1
-    FROM
-    (SELECT {{ src_pk }},
-            {{ 'HASHDIFF' }},
-            ROW_NUMBER() OVER
-    (PARTITION BY {{ src_pk }}
-    ORDER BY {{ src_ldts }}) AS DV_RNK
 
+    {%- if is_any_incremental() %}
 
-    FROM DBTVAULT_DEV.TEST_CARL_BOYCE.RTS
-    QUALIFY DV_RNK = 1) CUR
-    WHERE {{ src_pk }} = CUR.{{ src_pk }}
-    AND {{ 'HASHDIFF' }} = CUR.{{ 'HASHDIFF' }})
---     {% endif %}
+    WHERE NOT EXISTS (
+        SELECT 1
+        FROM (
+            SELECT {{ src_pk }}, {{ src_hashdiff }},
+            ROW_NUMBER() OVER(
+                PARTITION BY {{ src_pk }}
+                ORDER BY {{ src_ldts }}
+            ) AS DV_RNK
+            FROM {{ this }}
+            QUALIFY DV_RNK = 1
+        ) AS cur
+    WHERE {{ src_pk }} = cur.{{ src_pk }}
+    AND {{ src_hashdiff }} = cur.{{ src_hashdiff }})
 
-
-
+    {% endif %}
 ),
 
 records_to_insert AS (
-    select distinct * from x AS a
-    {%- if dbtvault.is_vault_insert_by_period() or is_incremental() %}
+    SELECT {{ dbtvault.prefix(source_cols, 'a', alias_target='target') }}
+    FROM y AS a
+
+    {%- if is_any_incremental() %}
+
     LEFT JOIN {{ this }} AS d
+    ON a.{{ src_pk }} = d.{{ src_pk }}
+    AND a.{{ src_hashdiff }} = d.{{ src_hashdiff }}
+    WHERE a.{{ src_pk }} = d.{{ src_pk }}
+    AND a.{{ src_hashdiff }} = d.{{ src_hashdiff }}
 
-        ON a.{{ src_pk }} = d.{{ src_pk }}
-    AND (a.{{ 'HASHDIFF' }} = d.{{ 'HASHDIFF' }}
-
-    )
-    WHERE {{ dbtvault.prefix(['HASHDIFF'], 'd') }} IS NULL
-
-
-     {%- endif %}
-
-
+    {% endif %}
 
 )
 
-
-
- SELECT * FROM records_to_insert
+SELECT * FROM x
 
 {%- endmacro -%}
 
